d1ad52aa68e819ead60b49b8b40ed27be3506567
Adi Beća <adibecaAB@gmail.com>
2024-10-16 12:03:39 +0200

    Commit attempt no. 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,34 @@
 package org.example;
 
+
+/*Write a function called printInvertedTablesNumber that accepts three parameters
+of type int (start, end, number). Then the function goes from end number to the
+ start number and prints the multiplication table of the
+ numbers provided. If you call this function with following parameters the output
+ will be as follows printInvertedTablesNumber(1,10, 3);
+ */
+import java.sql.SQLOutput;
 import java.util.Scanner;
-public class Tasks {
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-        //TASK 1
-        System.out.println("Jane Doe");
-        //TASK 2
-        System.out.println("Hello world!");
-        System.out.println("(And all the people in the world)");
 
-        //TASK 3
-        System.out.println("There are "+365*24*60*60+" seconds in a year.");
+        drawStarsPyramid(5);
+        input.close(); // Close the scanner
+    }
 
-        //TASK 4
-        System.out.println("Type a number:");
-        int a=input.nextInt();
-        System.out.println("Type another number:");
-        int b=input.nextInt();
-        System.out.println("Sum of the numbers: "+(a+b));
+    public static void drawStarsPyramid(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *");
+            }
 
-        //TASK 5
-        System.out.println("Type a number: ");
-        int c=input.nextInt();
-        if(c>0){
-            System.out.println("Number is positive.");
-        }
-        else{
-            System.out.println("Number is not positive.");
-        }
-
-        //TASK 6
-        System.out.println("Type the first number: ");
-        int d=input.nextInt();
-        System.out.println("Type the second number: ");
-        int e= input.nextInt();
-        if(e==d){
-            System.out.println("The numbers are equal.");
-        }
-        else{
-            int bigger=(d>e)?d:e;
-            System.out.println("Greater number: "+bigger);
+            System.out.println(); // Move to the next line after printing stars
         }
     }
 }
+
+
